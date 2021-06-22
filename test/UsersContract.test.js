ed3ad552758dbe23bfc72278af639578c333fce1
TEGDV <apjair97@gmail.com>
2021-06-21 22:42:09 -0500

    :white_check_mark: Created Users contract behaviors test

--- test/UsersContract.test.js
+++ test/UsersContract.test.js
@@ -1,6 +1,6 @@
 const assert = require('assert');
 const Web3 = require('web3');
-
+const AssertionError = require('assert').AssertionError;
 const web3 = new Web3("HTTP://127.0.0.1:7545");
 
 const {abi, bytecode} = require("../scripts/compile");
@@ -16,7 +16,58 @@ beforeEach(async() => {
 
 describe('The UsersContract', async () => {
   it('Should deploy', async () => {
-    console.log(usersContract.options.address);
     assert.ok(usersContract.options.address);
   })
+  
+    it('should join user', async () => {
+      let name = "Jair";
+      let surName = "Aguilar";
+      await usersContract.methods.join(name,surName)
+        .send({from:accounts[0],gas:'500000'});
+    })
+  
+    it('should retrieve a user', async () => {
+      
+      let name = "Jair";
+      let surName = "Aguilar";
+
+      await usersContract.methods.join(name,surName)
+        .send({from:accounts[0],gas:'500000'});
+
+      let user = await usersContract.methods.getUser(accounts[0]).call();
+      assert.equal(name, user[0])
+      assert.equal(surName, user[1])
+
+    })
+  
+  it('should not allow join twice', async () => {
+      await usersContract.methods.join("Chuponsito","El Payaso")
+        .send({from:accounts[1],gas:'500000'})
+      try{
+        await usersContract.methods.join("Platanito","Chow")
+          .send({from:accounts[1],gas:'500000'})
+        assert.fail("same account can't join twice");
+      }
+    catch(e){
+      e instanceof AssertionError && assert.fail(e.message);
+    }
+    })
+
+  
+    it('User lenght returns the correct number of users', async () => {
+      await usersContract.methods.join("Chuponsito","El Payaso")
+        .send({from:accounts[1],gas:'500000'})
+      let userLength = await usersContract.methods.totalUsers().call()
+      assert.equal(userLength, 1)
+    })
+  
+    it('Joined users should be detected', async () => {
+      await usersContract.methods.join("Chuponsito","El Payaso")
+        .send({from:accounts[1],gas:'500000'})
+
+      let isJoined = await usersContract.methods.userJoined(accounts[1]).call();
+      assert(isJoined);
+      let isNotJoined = await usersContract.methods.userJoined(accounts[2]).call();
+      assert(!isNotJoined);
+    })
 })
